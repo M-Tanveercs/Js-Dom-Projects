643fa9d2b85f844924044696af155de8124845f8
Muhammad Tanveer <75263145+M-Tanveercs@users.noreply.github.com>
2023-12-20 10:15:22 +0500

    ultimate background color generator  in js

---
+++ 6-unlimitedColors/main.js
@@ -0,0 +1,35 @@
+
+
+
+
+let randomColor = function(){
+    let hex='0123456789ABCDEF'
+    let color='#'
+    for (let i = 0; i < 6; i++) {
+color += hex[Math.floor(Math.random()*16)]
+        
+}
+console.log(color);
+    return color
+}
+
+
+let intervalId
+
+const startChaningColor=function(){
+    if(!intervalId){
+        intervalId=setInterval(changeBackground,1000)
+    }
+    function changeBackground(){
+        document.body.style.backgroundColor=randomColor()
+    }
+}
+
+const stopChangingColor=function(){
+    clearInterval(intervalId);
+    intervalId=null
+}
+
+
+document.querySelector("#start").addEventListener('click',startChaningColor)
+document.querySelector('#stop').addEventListener('click',stopChangingColor)
